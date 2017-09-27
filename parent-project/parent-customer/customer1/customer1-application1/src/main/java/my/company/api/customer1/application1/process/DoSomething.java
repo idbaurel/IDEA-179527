455051010be0bb78c277f019296ae0b02283c695
aurelien.leroux <aurelien.leroux@infodb.fr>
2017-09-27 14:48:20 +0200

    init structure and source code

---
+++ parent-project/parent-customer/customer1/customer1-application1/src/main/java/my/company/api/customer1/application1/process/DoSomething.java
@@ -0,0 +1,39 @@
+package my.company.api.customer1.application1.process;
+
+import my.company.api.module.business.logic1.unit1.dto.*;
+
+/**
+ * @author aurelien
+ * créé le 27/09/17.
+ */
+public class DoSomething {
+
+    public AdditionalDto do0() {
+        return null;
+    }
+
+    public AcceptationPropositionPlanAideAsgDTO do1() {
+        return null;
+    }
+
+    public ConsultationDroitAsgResponseDTO do2() {
+        return null;
+    }
+
+    public FileUploadResponseDTO do3() {
+        return null;
+    }
+
+    public GenerationCodeConfidentielResponseDTO do4() {
+        return null;
+    }
+
+    public IndividuResponseDTO do5() {
+        return null;
+    }
+
+    public PropositionPlanAideAsgResponseDTO do6() {
+        return null;
+    }
+
+}
---
+++ parent-project/parent-customer/customer1/customer1-application1/src/main/java/my/company/api/module/business/logic1/unit1/dto/AdditionalDto.java
@@ -0,0 +1,9 @@
+package my.company.api.module.business.logic1.unit1.dto;
+
+/**
+ * @author aurelien
+ * créé le 27/09/17.
+ */
+public class AdditionalDto {
+
+}
